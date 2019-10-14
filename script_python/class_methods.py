22856550282d41fce43342676c6d0532a03da38b
paololeo1000 <paul.2000@tiscali.it>
2019-10-14 08:50:42 +0200

    new day

---
+++ script_python/class_methods.py
@@ -0,0 +1,15 @@
+class Television:
+    """ Class describing a television.
+    """
+    def turn_on(self, channel=1): # Class method
+        """ All the class methods get the object instace as their first 
+        argument. It is customary to call this argument 'self', though is not
+        required by the language rules (you can call it 'pippo' and it will work
+        just as well)
+        """
+        print('Showing channel {}'.format(channel))
+
+tv = Television()
+# Class methods and members are accessed through the '.' (dot) operator
+# You don't have to pass manually the self argument, it is added for you!
+tv.turn_on(channel=3)
