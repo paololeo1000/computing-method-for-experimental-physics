61061f37f1bebe342cfa0fe98a04b9ff52d1d8ac
paololeo1000 <paul.2000@tiscali.it>
2019-10-16 15:48:27 +0200

    module numpy

---
+++ script_python/numpy_arrays_vs_lists.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+# arrays and lists seem similiar...
+l = [1., 2., 3.]
+a = np.array(l)
+print(l)
+print(a)
+
+# ...but they support basic arithmetic in a different fashion
+print(l + l)
+print(a + a)
+
